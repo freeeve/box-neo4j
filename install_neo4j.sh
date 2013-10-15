3884fc2a5210c040c6012a6d1bf7a9a16bf7e7bd
Wes Freeman <wes@skeweredrook.com>
2013-10-15 19:35:11 -0400

    maybe we don't need the script?

--- install_neo4j.sh
+++ install_neo4j.sh
@@ -1,10 +1,1 @@
 #!/bin/bash
-NEO4J_VERSION=2.0.0-M06
-echo "neo4j version: $NEO4J_VERSION"
-DIR="neo4j-community-$NEO4J_VERSION"
-FILE="$DIR-unix.tar.gz"
-wget http://dist.neo4j.org/$FILE
-tar xvfz $FILE &> /dev/null
-rm $FILE
-cd $DIR/bin
-./neo4j start
